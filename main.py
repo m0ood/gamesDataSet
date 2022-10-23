a321b32e8ac6ce4511d6cd5348361523fc33446b
Aspectio <1dankmemesxd@gmail.com>
2022-10-23 21:18:40 +0300

    t

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import matplotlib
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('PyCharm')
-    df = pd.read_csv('https://raw.githubusercontent.com/jorisvandenbossche/pandas-tutorial/master/data/titanic.csv')
-    df
+    videogames = pd.read_csv("./vgsales.csv")
+    print(videogames)
+    GenceAndGlobal_Sales = pd.crosstab(videogames["Genre"], videogames["Global_Sales"], margins=True)
+    sns.set(rc={"figure.figsize": (15, 4)})
+    chart = sns.barplot(x=GenceAndGlobal_Sales["All"][:12].index, y=GenceAndGlobal_Sales["All"][:12])
+    print(chart.set(xlabel='Genre', ylabel='Sales(in millions)'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
