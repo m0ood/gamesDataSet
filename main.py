22730abbea9c1c05b61fb4cd45658573c63d4b42
Aspectio <1dankmemesxd@gmail.com>
2022-10-23 21:00:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hi('PyCharm')
+    df = pd.read_csv('https://raw.githubusercontent.com/jorisvandenbossche/pandas-tutorial/master/data/titanic.csv')
+    df
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
